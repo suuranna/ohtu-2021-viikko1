a38ef22fc34ba1064fed4215d4ef935b0f0e71c9
Anna Suuronen <annasuuronen@icloud.com>
2021-11-13 02:18:15 +0200

    tehtävä 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,18 +5,32 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
+        self.varasto2 = Varasto(-1)
 
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_alku_saldon_muuttaminen_negatiiviseksi(self):
+        varasto1 = Varasto(15, -100)
+        self.assertAlmostEqual(varasto1.saldo, 0)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+        self.assertAlmostEqual(self.varasto2.tilavuus, 0.0)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisays_ei_lisaa_negatiivista(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_tulostus(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +52,19 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_liian_suuren_maaran_lisaaminen(self):
+        self.varasto.lisaa_varastoon(100)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_varastosta_negatiivinen(self):
+        arvo = self.varasto.ota_varastosta(-11)
+        self.assertAlmostEqual(arvo, 0.0)
+
+    def test_ota_varastosta_kaikki(self):
+        self.varasto.lisaa_varastoon(6)
+        arvo = self.varasto.ota_varastosta(1000)
+        self.assertAlmostEqual(arvo, 6)
+        self.assertAlmostEqual(self.varasto.saldo, 0.0)
+
+ 
